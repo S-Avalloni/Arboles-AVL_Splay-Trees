e75ab49ec3a57e3604befedb59099b17da06434c
Santiago Avalloni <104738448+S-Avalloni@users.noreply.github.com>
2026-06-18 10:22:07 -0400

    Añade firmas a trees.h

--- include/trees.h
+++ include/trees.h
@@ -4,10 +4,14 @@
 #include "./avl.h"
 #include "./splay.h"
 
+// Escape codes para poder poner colores en linea de comandos
 #define BLUE(string) "\x1b[34m" string "\x1b[0m"
 #define GREEN(string) "\x1b[32m" string "\x1b[0m"
 #define RED(string) "\x1b[31m" string "\x1b[0m"
 
+// Funciones genericas para facilitar el uso en los
+// codigos de src/. Revisar comentarios especificos
+// en avl.h o splay.h
 #define zig(arbol) _Generic((arbol), \
   Avl*: zig_avl, \
   SplayTree*: zig_spl \
@@ -19,27 +23,8 @@
 )(arbol)
 
 /*
-Funciones no implementadas en avl por lo que no
-es necesario hacerlas genéricas
-#define zigzig(arbol) _Generic((arbol), \
-  Avl*: zigzig_avl, \
-  SplayTree*: zigzig_spl \
-)(arbol)
-// 
-#define zagzag(arbol) _Generic((arbol), \
-  Avl*: zagzag_avl, \
-  SplayTree*: zagzag_spl \
-)(arbol)
-// 
-#define zigzag(arbol) _Generic((arbol), \
-  Avl*: zigzag_avl, \
-  SplayTree*: zigzag_spl \
-)(arbol)
-// 
-#define zagzig(arbol) _Generic((arbol), \
-  Avl*: zagzig_avl, \
-  SplayTree*: zagzig_spl \
-)(arbol)
+No fue necesario implementar el generico de ninguna
+rotación doble ya que Avl fue hecho sin estas implementaciones
 */
 
 #define search(arbol, i) _Generic((arbol), \
@@ -47,8 +32,6 @@ es necesario hacerlas genéricas
   SplayTree*: search_spl \
 )(arbol, i)
 
-// inserta el elemento i al arbol y retorna un puntero a la
-// cabeza de este
 #define insert(ctx, arbol, i) _Generic((ctx), \
   AvlContext*: insert_avl, \
   SplayTreeContext*: insert_spl \
@@ -69,23 +52,26 @@ es necesario hacerlas genéricas
   SplayTreeContext*: delete_spl \
 )(ctx)
 
+// Función de comparación de unsigned int para
+// utilizar en qsort de una lista con unsigned
+// int, -1 si a<b, 1 si a>b, 0 si a==b
 int compare_asc(const void *a, const void *b);
 
 typedef int (*Generador)(unsigned int N);
 
-// Se va a tomar lambda = log(31/32) ~ 0.014 ya que así
-// se facilita el calculo de la probabilidad y el sampleo
-// de los datos.
-// Esto implica que exp(-lambda) = 31/32
-// La distribución que nos dan es una geometrica de
-// parametro 1-exp(-lambda) = 1/32
+// Habiendo elegido un buen lambda esta es una geometrica
+// de prametro 1/32 en el rango [0, N-1]
+// Por rand(), N debe ser menor o igual a 2^31
 int geom(unsigned int N);
 
+// Variable aleatoria uniforme en rango [0, N-1]
+// implementado con rand() por lo que N debe ser
+// menor o igual a 2^31 para que tenga sentido
 int unif(unsigned int N);
 
+// Función identidad con la misma forma que los otros
+// generadores para facilitar la toma de numeros en orden
 int seq(unsigned int N);
-
-void to_array(Avl *arbol, unsigned int *lista, int *idx);
 
 
 #endif
